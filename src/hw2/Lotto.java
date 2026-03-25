415ee6cf446ee434add0108522246f806a8de14f
Annie <appleon918@gmail.com>
2026-03-25 20:14:07 +0800

    樂透

---
+++ src/hw2/Lotto.java
@@ -0,0 +1,18 @@
+package hw2;
+
+public class Lotto {
+	public static void main(String[] args) {
+		int count =0;
+		for(int i =1;i<=49;i++) {
+			if(i%10!=4&&i/10!=4) {
+				System.out.println(i+" ");
+				count++;
+				
+				
+			}
+		}
+		System.out.println("總共有"+count+"個數可選");
+	
+	}
+
+}
