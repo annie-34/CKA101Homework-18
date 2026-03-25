96b3324015c8e00700afc311a7e521a1bf321b3e
Annie <appleon918@gmail.com>
2026-03-25 19:49:18 +0800

    new number

--- src/hw2/Number.java
+++ src/hw2/Number.java
@@ -1,15 +1,11 @@
 package hw2;
 
 public class Number {
-	int count=0;
-	
-	public static void number() {
-		for(int i=1;i<=100;i++) {
-		for(int j =3)
-			
-		}
-	}
+
 public static void main(String[]args) {
+	for(int i =1;i<10;i++) {
+		System.out.print(i * i + " ");
+	}
 
 	
 }
